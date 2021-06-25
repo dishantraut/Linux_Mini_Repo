3865dcbcbcfa424afcf90066a1805cb734673f9f
Dishant Raut <dishantraut@gmail.com>
2021-06-26 00:58:47 +0530

    Time Date

---
+++ time_date.sh
@@ -0,0 +1,11 @@
+# * Parsing Date & Time
+year=$(date +%Y)
+month=$(date +%m)
+day=$(date +%d)
+hour=$(date +%H)
+minute=$(date +%M)
+second=$(date +%S)
+echo $(date)
+echo "Current Date is: $day-$month-$year"
+echo "Current Time is: $hour:$minute:$second"
+
