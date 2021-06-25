62200fe77a5636e45b394450b6257d4f8c98f7bf
dishantraut <63045109+dishantraut@users.noreply.github.com>
2021-06-26 02:36:55 +0530

    Update system_maintenance.sh

--- system_maintenance.sh
+++ system_maintenance.sh
@@ -2,6 +2,7 @@
 echo -e "\n$(date "+%d-%m-%Y --- %T") --- Starting work\n"
 sudo apt-get update
 sudo apt-get -y upgrade
+sudo apt-get -y full-upgrade
 sudo apt-get -y autoremove
 sudo apt-get autoclean
 echo -e "\n$(date "+%T") \t Script Terminated"
