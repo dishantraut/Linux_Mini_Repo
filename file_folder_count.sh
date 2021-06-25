7a8947a09a26c93134e4bd2dd26f2e74384770fa
Dishant Raut <dishantraut@gmail.com>
2021-06-26 00:52:10 +0530

    Get File & Folder Count

---
+++ file_folder_count.sh
@@ -0,0 +1,9 @@
+# * Print number of files or directories
+if [ -d "$@" ]; then
+	    echo "Files found: $(find "$@" -type f | wc -l)"
+	        echo "Folders found: $(find "$@" -type d | wc -l)"
+	else
+		    echo "[ERROR] Please retry with another folder."
+		        exit 1
+fi
+
