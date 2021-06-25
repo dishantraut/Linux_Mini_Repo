b5c2e939e8201ba18322791d1541da11672a2f92
Dishant Raut <dishantraut@gmail.com>
2021-06-26 01:04:10 +0530

    Append to existing file

---
+++ append_to_file.sh
@@ -0,0 +1,8 @@
+# * Append to files
+# * Make file "editors.txt" before testing
+echo "Before appending the file"
+cat editors.txt
+echo "6. NotePad++" >>editors.txt
+echo "After appending the file"
+cat editors.txt
+
